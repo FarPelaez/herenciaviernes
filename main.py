5b27994a228e505c39373df4441ca2e1ee4c0758
FarPelaez <farley.diseno@gmail.com>
2022-04-29 20:45:22 -0500

    Ejemplificando la Herencia

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from clases.Cliente import Cliente
 # Crear un objeto de la clase hija
 hijo = Cliente()
 
+# Asignar valores a la clase hija
 hijo.nombre = "Juan José Ardila Lulle"
 hijo.saldo = 5000
 hijo.clasificar()
