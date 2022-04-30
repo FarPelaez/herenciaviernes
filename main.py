c84a6a740b959ac241807664d11c9d54d86912f0
FarPelaez <farley.diseno@gmail.com>
2022-04-29 20:43:47 -0500

    Herencia

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#Herencia en python
+# Herencia en python
+from clases.Cliente import Cliente
+
+# Crear un objeto de la clase hija
+hijo = Cliente()
+
+hijo.nombre = "Juan José Ardila Lulle"
+hijo.saldo = 5000
+hijo.clasificar()
